49b3efd1480dd3a478133bedd9da321cc835cbaa
Stanislav Sporevoi <stanislav.sporevoi@gmail.com>
2022-09-23 00:17:59 +0300

    Update App.jsx

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,7 +1,8 @@
 import { Component } from "react";
+import * as API from '../services/api';
 import Searchbar from "./Searchbar"
 // import ImageGellary from "./ImageGallery";
-import * as API from '../services/api';
+
 
 
 export class App extends Component {
@@ -16,7 +17,7 @@ export class App extends Component {
 
   componentDidUpdate(_, prevState) {
     const { page, searchQuerry } = this.state;
-    if ( searchQuerry && prevState.searchQuerry !== searchQuerry||prevState.page !== page ) {
+    if ( (searchQuerry && prevState.searchQuerry !== searchQuerry) || prevState.page !== page ) {
       this.getImages(searchQuerry, page);
       }
   }
